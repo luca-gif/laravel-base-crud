4d1a61ce43cbcfd3e5433118799429dc9b23fd80
luca-gif <luca.89m@gmail.com>
2022-06-29 18:26:49 +0200

    inserito tasto show

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -48,7 +48,7 @@ class ComicsController extends Controller
      */
     public function show($id)
     {
-        //
+        dd($id);
     }
 
     /**
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,11 @@
                     @foreach ($comics as $comic)
                         <div class="col-4 card">
                             <img src="{{ $comic->image }}" alt="">
-                            <li>{{ $comic->title }}</li>
-                            <li>{{ $comic->type }}</li>
+                            <div class="text">
+                                <li>{{ $comic->title }}</li>
+                                <li>{{ $comic->type }}</li>
+                            </div>
+                            <a class="btn btn-danger" href="{{ route('index.show', $comic) }}">Show</a>
                         </div>
                     @endforeach
                 </div>
